f619066e77456ea0879a8575802ff03b29a79170
radumarica <radunicolaemarica@gmail.com>
2022-09-18 12:24:03 +0300

    hover issue

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -60,7 +60,7 @@ function MyApp({ Component, pageProps }) {
 
 
 
-                      <h3 onClick={() => setShowDrop(!showDrop)} onMouseEnter={() => setShowDrop(true)} className="hover:text-[#13D511] text-2xl text-[#ECF7FD] transition-all font-semibold cursor-pointer ">
+                      <h3 onClick={() => setShowDrop(!showDrop)} onMouseOver={() => setShowDrop(true)} onMouseOut={() => setShowDrop(false)} className="hover:text-[#13D511] text-2xl text-[#ECF7FD] transition-all font-semibold cursor-pointer ">
                         What We Build
                       </h3>
 
